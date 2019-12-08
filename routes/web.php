0e83517adb23d0d8884a47e70f3e3f3284f4f5dd
Todd Austin <austin.todd.j@gmail.com>
2019-12-08 14:58:16 -0600

    added verification routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,6 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('verified')->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
