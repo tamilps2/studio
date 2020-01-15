d7d5f098a9e1cb2e8753405212fab182dcffeff6
Todd Austin <austin.todd.j@gmail.com>
2020-01-15 06:28:33 -0600

    Apply fixes from StyleCI (#4)

--- config/secure-headers.php
+++ config/secure-headers.php
@@ -472,7 +472,7 @@ return [
 
         'script-src' => [
             'allow' => [
-                'cdn.jsdelivr.net'
+                'cdn.jsdelivr.net',
             ],
 
             'hashes' => [
